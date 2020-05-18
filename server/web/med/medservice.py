52870e38e042320619bfb9343bda3f800ef3c5b5
Prithvi Raj <prithvi@xlayer.in>
2020-05-18 16:48:46 +0530

    cancellation of appointment by customer

--- server/web/med/medservice.py
+++ server/web/med/medservice.py
@@ -497,6 +497,86 @@ class MedServiceBookHandler(cyclone.web.RequestHandler,
                                 code = 4070
                                 status = False
                                 message = "Invalid Option"
+                        elif self.apiId == 502020:
+                            try:
+                                bookingId = ObjectId(self.request.arguments.get('bookingId'))
+                            except:
+                                code = 4050
+                                message = "Invalid Booking Id"
+                                raise Exception
+                            serBook = yield self.serviceBook.find(
+                                        {
+                                            '_id':bookingId
+                                        }
+                                    )
+                            if not len(serBook):
+                                code = 4060
+                                message = "Invalid Booking"
+                                raise Exception
+                            phoneNumber = serBook[0]['accountDetails'][0]['contact'][0]['value']
+                            # TODO: Country code hard coded
+                            phoneNumber = str(phoneNumber - 910000000000)
+                            Log.i('Customer Phone Number', phoneNumber)
+
+                            serList = yield self.serviceList.find(
+                                        {
+                                            '_id':serBook[0]['serviceId']
+                                        }
+                                    )
+                            if not len(serList):
+                                code = 4060
+                                message = "Invalid Service"
+                                raise Exception
+
+                            serName = serList[0]['serNameEnglish']
+
+                            serUpdate = yield self.serviceBook.update(
+                                        {
+                                            '_id':bookingId
+                                        },
+                                        {
+                                        '$set':{
+                                                    'stage':declined
+                                                }
+                                        }
+                                    )
+                            sms = 'Greetings from Ohzas! We are sorry to know that you \
+                                    have cancelled the appointment for {}. \
+                                    We look forward to provide service to you again.'.format(serName)
+                            if serUpdate['n']:
+                                conn = http.client.HTTPSConnection("api.msg91.com")
+                                payloadJson = {
+                                                "sender":"SOCKET",
+                                                "route":4,
+                                                "country":91,
+                                                "sms":[
+                                                        {
+                                                            "message":sms,
+                                                            "to":[phoneNumber]
+                                                        }
+                                                    ]
+                                                }
+                                payload = json.dumps(payloadJson)
+                                headers = {
+                                            'authkey': MSG91_GW_ID,
+                                            'content-type': "application/json"
+                                        }
+                                conn.request("POST", "/api/v2/sendsms", payload, headers)
+                                res = conn.getresponse()
+                                data = res.read()
+                                stat = json.loads(data.decode("utf-8"))
+                                Log.i('Notification Status',stat['type'])
+                                if stat['type'] == "success":
+                                    code = 2000
+                                    message = "Request has been cancelled"
+                                    status = True
+                                else:
+                                    code = 4055
+                                    Log.i('SMS notification could not be sent')
+                            else:
+                                code = 2000
+                                message = "Invalid booking"
+                                status = True
                         else:
                             code = 4003
                             self.set_status(401)
